47a8e13a713cc109e3d61072fbfeb2d4dfcf92db
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-02-04 14:24:31 +0100

    create dark-mode component

---
+++ frontend/src/components/DarkMode.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <button class="btn btn-primary" @click="toggleDarkMode"> </button>
+
+
+</template>
+
+
+<script>
+    export default {
+        name: "DarkMode",
+        props: {
+            darkMode: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            toggleDarkMode() {
+                this.$emit('dark-mode-change',!this.darkMode);
+            }
+        }
+    }
+
+</script>
